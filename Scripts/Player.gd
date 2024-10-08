7ac3b22318cc806b3e9647e0391b5a82b1ebca93
DillonB07 <dillonb07dev@gmail.com>
2024-10-08 15:54:15 +0100

    animation stuffs

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @export var speed = 300.0
 @export var gravity = 200
+@export var jump_height = -100
 
 
 func horizontal_movement():
@@ -11,4 +12,22 @@ func horizontal_movement():
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 	horizontal_movement()
-	move_and_slide()
+	#move_and_slide()
+	player_animations()
+
+func player_animations():
+	if Input.is_action_pressed("ui_left")|| Input.is_action_just_released("ui_jump"):
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.play("run")
+		
+	if Input.is_action_pressed("ui_right") || Input.is_action_just_released("ui_jump"):
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play("run")
+		
+	if !Input.is_anything_pressed():
+		$AnimatedSprite2D.play("idle")
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_jump") and is_on_floor():
+		velocity.y = jump_height
+		$AnimatedSprite2D.play("jump")
